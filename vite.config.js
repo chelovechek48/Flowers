ee07c8b7f4cb5cbd4fd56452ef0d9886833192f5
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-03-05 22:27:24 +0300

    change base url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/<REPO>"
-  base: '/new-vite-vue',
+  base: '/Flowers',
   server: {
     port: '8080',
   },
