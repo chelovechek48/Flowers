a5d136f65b9bed5f2e92a2f266a6e264d87e1227
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-03-06 14:20:57 +0300

    Added banner

---
+++ src/components/BannerSwiper.vue
@@ -0,0 +1,73 @@
+<script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Autoplay],
+    };
+  },
+};
+</script>
+
+<template>
+  <Swiper
+    class="swiper"
+    :loop="true"
+    slides-per-view="auto"
+    space-between="12"
+
+    :modules="modules"
+    :autoplay="{delay: 2500}"
+  >
+    <Swiper-slide class="slide">
+      <a href="">
+        <img
+          class="image"
+          src="@images/banner/букет-невест@1.25x.jpg"
+          alt=""
+        >
+      </a>
+    </Swiper-slide>
+    <Swiper-slide class="slide">
+      <a href="">
+        <img
+          class="image"
+          src="@images/banner/букет-невест@1.25x.jpg"
+          alt=""
+        >
+      </a>
+    </Swiper-slide>
+    <Swiper-slide class="slide">
+      <a href="">
+        <img
+          class="image"
+          src="@images/banner/букет-невест@1.25x.jpg"
+          alt=""
+        >
+      </a>
+    </Swiper-slide>
+  </Swiper>
+</template>
+
+<style lang="scss" scoped>
+@use '@vars/container';
+
+.swiper {
+  padding-inline: container.$padding;
+  margin-inline: (0 - container.$padding);
+}
+.slide {
+  max-width: 20rem;
+}
+.image {
+  width: 100%;
+  border-radius: 1rem;
+}
+</style>
--- src/components/FirstScreen.vue
+++ src/components/FirstScreen.vue
@@ -1,14 +1,17 @@
 <script setup>
 import SearchPanel from '@components/SearchPanel.vue';
+import BannerSwiper from '@components/BannerSwiper.vue';
 </script>
 
 <template>
-  <section>
+  <section class="fiter-screen">
     <SearchPanel />
-    <aside class="banner" />
+    <BannerSwiper />
   </section>
 </template>
 
 <style lang="scss" scoped>
-
+.fiter-screen {
+  padding-top: 70px;
+}
 </style>
