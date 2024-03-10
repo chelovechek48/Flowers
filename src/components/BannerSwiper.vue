f1f66ac827414e2e22ba6df80530c741c513d7d1
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-03-10 17:42:50 +0300

    Try webp

--- src/components/BannerSwiper.vue
+++ src/components/BannerSwiper.vue
@@ -12,7 +12,7 @@ const obj = {
     // Возвращаем результат динамического импорта изображения
     images[path]().then((module) => module.default)),
 };
-console.log(Object.keys(images)[0]);
+console.log(Object.keys(images)[1]);
 
 const url = async (path) => {
   try {
