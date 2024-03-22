b2c7f361238c89ca6f00580329945715884c3e8c
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-03-22 17:16:09 +0300

    try second sprite

--- src/components/ButtonSVG.vue
+++ src/components/ButtonSVG.vue
@@ -1,7 +1,10 @@
 <script setup>
 import { defineProps } from 'vue';
 
-const importPath = (id) => `#${id}`;
+import spritePath from '@icons/sprite.svg';
+import spritePath2 from '@icons/sprite-feed.svg';
+
+const importPath = (id) => `${spritePath}#${id}`;
 
 defineProps({
   svgClassName: {
@@ -42,6 +45,12 @@ defineProps({
       :viewBox="`0 0 ${svgRatio}`"
     >
       <use :href="importPath(svgPath)" />
+    </svg>
+    <svg
+      :class="svgClassName"
+      :viewBox="`0 0 ${svgRatio}`"
+    >
+      <use :href="`${spritePath2}#feed`" />
     </svg>
     <span
       v-if="buttonText"
