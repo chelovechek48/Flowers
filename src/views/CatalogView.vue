302d266ac4fd7ddaa7708d724ee8f11024d7d847
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-03-22 16:58:11 +0300

    Try :xlink

--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -2,11 +2,12 @@
 import { ref } from 'vue';
 import products from '@/assets/data/products.json';
 import ProductCard from '@components/ProductCard.vue';
-import spritePath from '@icons/sprite-feed.svg';
+import '@icons/sprite-feed.svg';
 import { useRoute, useRouter } from 'vue-router';
 
 const productsPath = import.meta.glob('@images/products/*.*');
-const importPath = (id) => `${spritePath}#${id}`;
+// const importPath = (id) => `${spritePath}#${id}`;
+const importPath = (id) => `#${id}`;
 
 const filterItems = [];
 products.forEach((prod) => {
@@ -69,7 +70,7 @@ const feed = ref(true);
             :for="it"
           >
             <svg class="choicer__icon">
-              <use xlink:href="@icons/sprite-feed.svg#feed" />
+              <use :xlink:href="`/Flowers/src/assets/icons/sprite-feed.svg#${it}`" />
             </svg>
           </label>
         </div>
