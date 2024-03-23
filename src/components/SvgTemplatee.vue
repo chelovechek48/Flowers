fbfc21f8f6b9ad8942a6a46bb1f0198e05197e9b
Maksim Ivanyukhin <chelovechek.maks@mail.ru>
2024-03-23 17:01:23 +0300

    rename svg template

---
+++ src/components/SvgTemplatee.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { defineProps } from 'vue';
+import sprite from '@icons/sprite.svg';
+
+defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <svg>
+    <use :href="`${sprite}#${id}`" />
+  </svg>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
